35278cb5ed916f24088bed72bef097746179d9c5
Pravalikaa18 <112331028+Pravalikaa18@users.noreply.github.com>
2025-02-20 10:44:28 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,15 @@
+public class Palindrome {
+    public static boolean isPalindrome(String str) {
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equalsIgnoreCase(reversed);
+    }
+
+    public static void main(String[] args) {
+        String testStr = "madam";
+        if (isPalindrome(testStr)) {
+            System.out.println(testStr + " is a palindrome.");
+        } else {
+            System.out.println(testStr + " is not a palindrome.");
+        }
+    }
+}
